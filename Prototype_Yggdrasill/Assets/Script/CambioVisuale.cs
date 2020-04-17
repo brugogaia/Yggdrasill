6b4b9dee6ea0c6366c1dbcdaec702aaa34334d95
Mara <mara.lupano@libero.it>
2020-04-17 14:27:43 +0200

    Cambio visuale della camera (attenzione che in 3D si muove ancora con i comandi 2D)

---
+++ Prototype_Yggdrasill/Assets/Script/CambioVisuale.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CambioVisuale : MonoBehaviour
+{
+    //DA APPLICARE SUL PIANO POSTO TRA LE DUE SEQUENZE
+
+    GameObject Camera;
+    bool visuale3D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Camera = GameObject.FindGameObjectWithTag("MainCamera");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other = GameObject.FindGameObjectWithTag("Player").GetComponent<Collider>())
+        {
+            visuale3D = Camera.GetComponent<Camera>().getVisual();
+            if (!visuale3D) visuale3D = true;
+            Camera.GetComponent<Camera>().ChangeVisual(visuale3D);
+        }
+    }
+}
--- Prototype_Yggdrasill/Assets/Script/Camera.cs
+++ Prototype_Yggdrasill/Assets/Script/Camera.cs
@@ -5,15 +5,42 @@ using UnityEngine;
 public class Camera : MonoBehaviour
 {
     public Transform target;
+    public GameObject Player;
+    public GameObject MainCamera;
     public float smoothTime = 0.3F;
     private Vector3 velocity = Vector3.zero;
+    bool visual3D = false;
+
+    private void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+        MainCamera = GameObject.FindGameObjectWithTag("MainCamera");
+    }
 
     void Update()
     {
+        if (!visual3D) target = GameObject.FindGameObjectWithTag("Target2D").transform;
+        else
+        {
+            target = GameObject.FindGameObjectWithTag("Target3D").transform;
+            
+        }
         // Define a target position above and behind the target transform
         Vector3 targetPosition = target.TransformPoint(new Vector3(0, 5, -10));
 
         // Smoothly move the camera towards that target position
         transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+    }
+
+    public void ChangeVisual(bool visuale3D)
+    {
+        visual3D = visuale3D;
+        Debug.Log("Visuale cambiata");
+        transform.Rotate(0, 45, 0);
+    }
+
+    public bool getVisual()
+    {
+        return visual3D;
     }
 }
