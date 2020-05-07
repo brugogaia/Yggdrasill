1c7382e74f3c419b550a57189896e9b54b3d31e0
Mara <mara.lupano@libero.it>
2020-05-07 19:55:04 +0200

    Inserite un po' di cose (vedi descrizione)
    Inserita mamma Alice con UI, cambio scena con labirinto più grande e Alice che si muove in modo casuale. Quando è presa segue giocatore

---
+++ Prototype_Yggdrasill/Assets/Script/Alice.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Alice : MonoBehaviour
+{
+    private float speed = 5f;
+    private float waitTime = 2f;
+    private float timer = 2f;
+    private Transform player;
+    private Transform target;
+    private Vector3 position;
+    private bool presa = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        target = GameObject.FindGameObjectWithTag("TargetAlice").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer = timer + Time.deltaTime;
+        if (!presa)
+        {
+            if (timer > waitTime)
+            {
+                position = new Vector3(Random.Range(-1.0f, 1.0f), 0, Random.Range(-1.0f, 1.0f));
+
+                timer = 0.0f;
+            }
+            transform.Translate(position * Time.deltaTime * speed);
+        }
+        else
+        {
+            speed = 10f;
+            transform.position = Vector3.MoveTowards(transform.position, target.position, speed);
+            /*transform.parent = target.transform;
+            Object.Destroy(this.GetComponent<Rigidbody>());*/
+        }
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other == player.GetComponent<Collider>())
+        {
+            presa = true;
+            this.GetComponent<AudioSource>().enabled = false;
+        }
+    }
+}
---
+++ Prototype_Yggdrasill/Assets/Script/MammaAlice.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MammaAlice : MonoBehaviour
+{
+    public Transform player;
+    [SerializeField] Image UI_Image;
+    private bool arrivato = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        UI_Image.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (arrivato && Input.GetKeyDown(KeyCode.Z))
+        {
+            SceneManager.LoadScene("Labyrinth", LoadSceneMode.Single);
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other == player.GetComponent<Collider>())
+        {
+            UI_Image.enabled = true;
+            arrivato = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other == player.GetComponent<Collider>())
+        {
+            UI_Image.enabled = false;
+            arrivato = false;
+        }
+    }
+}
