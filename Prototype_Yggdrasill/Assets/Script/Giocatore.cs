63c5884da3c034e35ec1a05fb0bf2b45784eaadf
Mara <mara.lupano@libero.it>
2020-05-28 16:24:42 +0200

    Salto in corsa e altre animazioni corrette

--- Prototype_Yggdrasill/Assets/Script/Giocatore.cs
+++ Prototype_Yggdrasill/Assets/Script/Giocatore.cs
@@ -15,6 +15,11 @@ public class Giocatore : MonoBehaviour
     private Quaternion rotIniziale;
     private bool stavolando = false;
 
+    private float y_prec = 0f;
+    private float y_now = 0f;
+    private bool staDecollando = false;
+    private bool staAtterrando = false;
+
 
     private Transform enemy;
 
@@ -44,136 +49,172 @@ public class Giocatore : MonoBehaviour
         MenuPausa = GameObject.FindGameObjectWithTag("MenuPausa");
         healthbar = GameObject.FindGameObjectWithTag("HealthBar").transform;
         //anim = this.transform.GetComponentInChildren<Animator>();
+        y_now = transform.position.y;
+        anim.SetBool("Grounded", true);
     }
 
     void Update()
     {
-        if (!MenuPausa.GetComponent<MenuPausa>().pausa)
+        y_prec = y_now;
+        y_now = transform.position.y;
+        if (y_now > y_prec)
         {
-            enemy = this.GetComponent<PlayerShooting>().enemy;
-
-            this.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
-            this.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
-
-            if (stavolando && !Puu.GetComponent<Puu>().flying) Puu.GetComponent<Puu>().isFlying();
-
-            isDead = healthbar.GetComponent<HealthBar>().isDed();
-            if (isDead) Puu.GetComponent<Puu>().setNemico(null);
-            timer = timer + Time.deltaTime;
-            //Debug.Log("is grounded " + isGrounded);
-            if (Input.GetKeyDown("space") && isGrounded && !isDead )
+            staDecollando = true;
+            staAtterrando = false;
+            Debug.Log("sta decollando");
+        }
+        else if (y_now < y_prec)
+        {
+            staDecollando = false;
+            staAtterrando = true;
+            Debug.Log("Sta atterrando");
+        }
+        else if(y_now == y_prec)
+        {
+            staDecollando = false;
+            staAtterrando = false;
+        }
+        if (staAtterrando && transform.position.y < 15) anim.SetBool("Grounded", true);
+            if (!MenuPausa.GetComponent<MenuPausa>().pausa)
             {
-                Jump();
-            }
-            else if (Input.GetKeyDown("space") && !isGrounded && !isDead)
-            {
-                Debug.Log("sono nel volo");
-                Fly();
-                anim.SetBool("Flying", true);
+                enemy = this.GetComponent<PlayerShooting>().enemy;
 
-                if (k == 0)
+                this.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+                this.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+
+                if (stavolando && !Puu.GetComponent<Puu>().flying) Puu.GetComponent<Puu>().isFlying();
+
+                isDead = healthbar.GetComponent<HealthBar>().isDed();
+                if (isDead) Puu.GetComponent<Puu>().setNemico(null);
+                timer = timer + Time.deltaTime;
+                //Debug.Log("is grounded " + isGrounded);
+                if (Input.GetKeyDown("space") && isGrounded && !isDead)
                 {
-                    Puu.GetComponent<Puu>().isFlying();
-                    k++;
+                    Jump();
                 }
-            }
-            if (Input.GetKey("space") && stavolando && !isDead) Fly();
-            if (Input.GetKeyUp("space") && stavolando && !isDead)
-            {
-                stavolando = false;
-                anim.SetBool("Flying", false);
-                Puu.GetComponent<Puu>().StopFlying();
-                k = 0;
-            }
+                else if (Input.GetKeyDown("space") && !isGrounded && !isDead)
+                {
+                    Debug.Log("sono nel volo");
+                    Fly();
+                    anim.SetBool("Flying", true);
+
+                    if (k == 0)
+                    {
+                        Puu.GetComponent<Puu>().isFlying();
+                        k++;
+                    }
+                }
+                if (Input.GetKey("space") && stavolando && !isDead) Fly();
+                if (Input.GetKeyUp("space") && stavolando && !isDead)
+                {
+                    stavolando = false;
+                    anim.SetBool("Flying", false);
+                    Puu.GetComponent<Puu>().StopFlying();
+                    k = 0;
+                }
 
 
-            if (!isGrounded)
-            {
-                Atterra();
-            }
-            
-            
-            
-            speed = 50f;
-            //Debug.Log("k giocatore = " + k);
-            if (!vis3D)
-            {
+                if (!isGrounded)
+                {
+                    Atterra();
+                }
 
-                
-                    if(Input.GetKeyDown(KeyCode.D)|| Input.GetKeyDown(KeyCode.A))
+
+
+                speed = 50f;
+                //Debug.Log("k giocatore = " + k);
+                if (!vis3D)
+                {
+
+
+                    if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.A))
                     {
                         anim.SetBool("running", true);
                     }
-                    else if(Input.GetKeyUp(KeyCode.D)|| Input.GetKeyUp(KeyCode.A))
+                    else if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.A))
                     {
                         anim.SetBool("running", false);
                     }
-                
 
-                float movimentoOrizzontale = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
-                //Debug.Log(movimentoOrizzontale);
-                if (!isDead)
-                {
-                    
-                    if (enemy == null || (enemy!= null && !enemy.GetComponent<Enemy>().isLittle() /*&& Vector3.Distance(transform.position, enemy.position)>=20)*/ ) || (enemy!=null && enemy.GetComponent<Enemy>().isLittle()))
+
+                    float movimentoOrizzontale = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+                    //Debug.Log(movimentoOrizzontale);
+                    if (!isDead)
                     {
-                        transform.Translate(movimentoOrizzontale, 0, 0);
-                        if (movimentoOrizzontale > 0)
-                            GameObject.FindGameObjectWithTag("idle").transform.forward = Vector3.right;
-                        else if (movimentoOrizzontale == 0)
-                            GameObject.FindGameObjectWithTag("idle").transform.forward = GameObject.FindGameObjectWithTag("idle").transform.forward;
-                        else
-                            GameObject.FindGameObjectWithTag("idle").transform.forward = -Vector3.right;
-                    }
-                        
-                }
 
+                        if (enemy == null || (enemy != null && !enemy.GetComponent<Enemy>().isLittle() /*&& Vector3.Distance(transform.position, enemy.position)>=20)*/ ) || (enemy != null && enemy.GetComponent<Enemy>().isLittle()))
+                        {
+                            transform.Translate(movimentoOrizzontale, 0, 0);
+                            if (movimentoOrizzontale > 0)
+                                GameObject.FindGameObjectWithTag("idle").transform.forward = Vector3.right;
+                            else if (movimentoOrizzontale == 0)
+                                GameObject.FindGameObjectWithTag("idle").transform.forward = GameObject.FindGameObjectWithTag("idle").transform.forward;
+                            else
+                                GameObject.FindGameObjectWithTag("idle").transform.forward = -Vector3.right;
+                        }
+
+                    }
+
+                }
+                else
+                {
+
+                    float movimentoAvantiIndietro = Input.GetAxis("Vertical") * speed;
+                    float movimentoDxSx = Input.GetAxis("Horizontal") * speed;
+                    movimentoDxSx *= Time.deltaTime;
+                    movimentoAvantiIndietro *= Time.deltaTime;
+                    Vector3 AD = transform.right * movimentoAvantiIndietro;
+                    Vector3 DxSx = transform.forward * -movimentoDxSx;
+                    if (!isDead) transform.position = transform.position + AD + DxSx;
+
+
+
+                    Vector2 mouseMovement = new Vector2();
+                    mouseMovement.x = Input.GetAxis("Mouse X");
+                    mouseMovement.y = Input.GetAxis("Mouse Y");
+
+                    float angolo_orizzontale_rotazione = 0.0f;
+                    float angolo_verticale_rotazione = 0.0f;
+                    if (mouseMovement.magnitude > 0.05)
+                    {
+                        //Rotazione asse verticale camera
+                        angolo_verticale_rotazione -= mouseMovement.y * this.velocita_verticale_camera;
+
+                        //Rotazione asse orizzontale camera
+                        angolo_orizzontale_rotazione = mouseMovement.x * velocita_orizzontale_camera;
+                    }
+
+
+                    transform.Rotate(0, angolo_orizzontale_rotazione, 0);
+                    //transform.Rotate(0, 0, angolo_verticale_rotazione);
+                }
             }
             else
             {
-
-                float movimentoAvantiIndietro = Input.GetAxis("Vertical") * speed;
-                float movimentoDxSx = Input.GetAxis("Horizontal") * speed;
-                movimentoDxSx *= Time.deltaTime;
-                movimentoAvantiIndietro *= Time.deltaTime;
-                Vector3 AD = transform.right * movimentoAvantiIndietro;
-                Vector3 DxSx = transform.forward * -movimentoDxSx;
-                if (!isDead) transform.position = transform.position + AD + DxSx;
-
-
-
-                Vector2 mouseMovement = new Vector2();
-                mouseMovement.x = Input.GetAxis("Mouse X");
-                mouseMovement.y = Input.GetAxis("Mouse Y");
-
-                float angolo_orizzontale_rotazione = 0.0f;
-                float angolo_verticale_rotazione = 0.0f;
-                if (mouseMovement.magnitude > 0.05)
-                {
-                    //Rotazione asse verticale camera
-                    angolo_verticale_rotazione -= mouseMovement.y * this.velocita_verticale_camera;
-
-                    //Rotazione asse orizzontale camera
-                    angolo_orizzontale_rotazione = mouseMovement.x * velocita_orizzontale_camera;
-                }
-
-
-                transform.Rotate(0, angolo_orizzontale_rotazione, 0);
-                //transform.Rotate(0, 0, angolo_verticale_rotazione);
+                this.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
             }
-        }
-        else
-        {
-            this.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
-        }
         
         
     }
 
     private void Jump()
     {
-        this.GetComponent<Rigidbody>().AddForce(new Vector3(0f, 25f,0f), ForceMode.Impulse);
         anim.SetBool("Jump", true);
+        if (anim.GetBool("running"))
+        {
+            this.GetComponent<Rigidbody>().AddForce(new Vector3(0f, 25f, 0f), ForceMode.Impulse);
+        }
+
+        else
+        {
+            Invoke("ForzaSalto", 0.5f);
+        }
+        
+    }
+
+    private void ForzaSalto()
+    {
+        this.GetComponent<Rigidbody>().AddForce(new Vector3(0f, 14f,0f), ForceMode.Impulse);
     }
 
     private void Atterra()
@@ -238,6 +279,7 @@ public class Giocatore : MonoBehaviour
         if (collision.gameObject.tag == "Ground")
         {
             isGrounded = false;
+            anim.SetBool("Grounded", false);
         }
         else
         {
---
+++ Prototype_Yggdrasill/Assets/Script/PianoAtterraggio.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PianoAtterraggio : MonoBehaviour
+{
+    public Animator anim;
+    private GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other == player.GetComponent<Collider>())
+        {
+            Debug.Log("Passato giocatore");
+            anim.SetBool("Grounded", !anim.GetBool("Grounded"));
+        }
+    }
+}
